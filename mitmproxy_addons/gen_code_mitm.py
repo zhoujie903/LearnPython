bf30da750fe13b4f1418c60ffd115ae066dd334c
zhoujie <zhoujie_903@163.com>
2019-09-08 13:16:43 +0800

    feat：增加生成接口python代码的addon

---
+++ mitmproxy_addons/gen_code_mitm.py
@@ -0,0 +1,144 @@
+import json
+from urllib.parse import urlparse
+
+from mitmproxy import ctx
+from mitmproxy import flowfilter
+from mitmproxy import http
+
+'''
+生成接口python代码
+'''
+
+class GenCode(object):
+    def __init__(self):
+        ctx.log.info('__init__')
+
+        # 今日头条 - 
+        urls = [
+            'score_task/v1/task/page_data/',
+            'score_task/v1/task/sign_in/',
+            'score_task/v1/task/open_treasure_box/',
+            'score_task/v1/task/new_excitation_ad/',            
+            'score_task/v1/task/get_read_bonus/',
+            'score_task/v1/task/done_task/',
+            'score_task/v1/landing/add_amount/',             
+            'search/suggest/homepage_suggest/',
+            'search/suggest/initial_page/',
+            'api/news/feed/v47/',
+            'api/search/content/',           
+        ]
+        self.toutiao = flowfilter.parse('|'.join(urls))
+
+        # 火山极速版
+        urls = [
+            'luckycat/v1/task/page/',
+            'luckycat/v1/task/sign_in/',
+            'luckycat/v1/task/open_treasure_box/',
+            'luckycat/v1/task/done_task/',
+            'luckycat/v1/landing/add_amount/',
+            'luckycat/v1/task/get_read_bonus/',            
+        ]
+        self.huoshan = flowfilter.parse('|'.join(urls))
+
+        # 趣头条
+        urls = [
+            r'api-coin-service.aiclk.com/coin/service',     
+        ]
+        self.qu_tou_tiao = flowfilter.parse('|'.join(urls))        
+
+    def load(self, loader):
+        ctx.log.info('event: load')
+
+    def configure(self, updated):
+        ctx.log.info('event: configure')
+
+    def running(self):
+        ctx.log.info('event: running')
+
+    def done(self):
+        ctx.log.info('event: done')
+
+
+
+    def response(self, flow: http.HTTPFlow):
+        if self.huoshan(flow) or self.toutiao(flow) or self.qu_tou_tiao(flow):
+
+            request: http.HTTPRequest = flow.request
+
+            parse_result = urlparse(request.url)
+            url_path = parse_result.path
+
+            function_name = url_path.replace('/','_').strip('_')
+            headers_code = self.headers_string(flow)
+            params_code = self.params_string(flow)
+            data_code = self.data_string(flow) 
+
+            path = f'''/Users/zhoujie/Desktop/api/{function_name}.text'''  
+            with open(path, 'a') as f:
+                print(f'''# ---------------------''',file=f)
+
+                code = f'''
+def {function_name}(self):
+
+    {headers_code}
+
+    {params_code}
+
+    {data_code}
+
+    url = '{request.scheme}://{request.pretty_host}{url_path}'
+    result = self._{request.method.lower()}(url, headers=headers, params=params, data=data)
+    return result
+                
+'''
+                f.write(code)
+
+                print(f'''Response:''',file=f)
+                print(f'''{flow.response.text}''',file=f)
+                print(f'''# ---------------------\n\n''',file=f)
+
+    def headers_string(self, flow: http.HTTPFlow):
+        lines = ''
+        for key,value in flow.request.headers.items():
+            lines += f"\n\t\t'{key}': '{value}',"
+        s = f'''headers = {{{lines}\n\t}}'''        
+        return s
+
+
+    def params_string(self, flow: http.HTTPFlow):
+        lines = ''
+        for key,value in flow.request.query.items():
+            lines += f"\n\t\t'{key}': '{value}',"
+        s = f'''params = {{{lines}\n\t}}'''        
+        return s
+
+    def data_string(self, flow: http.HTTPFlow):
+        '''
+        Content-Type: application/x-www-form-urlencoded
+        Content-Type: application/json; charset=utf-8
+        Content-Type: text/plain;charset=utf-8
+        '''
+        lines = ''
+
+        # [urlencoded_form, multipart_form, plan, json]取其一
+        for key,value in flow.request.urlencoded_form.items():
+            lines += f"\n\t\t'{key}': '{value}',"
+
+        for key,value in flow.request.multipart_form.items():
+            lines += f"\n\t\t'{key}': '{value}',"
+
+        # Todo:复杂json数据还不能代码化
+        if 'application/json' in flow.request.headers.get('content-type',''):
+            d = json.loads(flow.request.text)
+            for key,value in d.items():
+                lines += f"\n\t\t'{key}': {value},"
+        
+        s = f'''data = {{{lines}\n\t}}'''        
+        return s
+
+
+    
+
+addons = [
+    GenCode()
+]
